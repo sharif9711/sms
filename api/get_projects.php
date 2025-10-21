5f3d27f6054575f33b0b6a0c1b380088f4524b6e
sharif9711 <sharif9711@naver.com>
2025-10-21 12:54:41 +0900

    Create get_projects.php

---
+++ api/get_projects.php
@@ -0,0 +1,32 @@
+<?php
+define('_GNUBOARD_', true);
+include_once '../../../common.php';
+header('Content-Type: application/json; charset=utf-8');
+
+if (!$is_member) {
+    echo json_encode(['success' => false, 'message' => '로그인이 필요합니다.']);
+    exit;
+}
+
+$mb_id = $member['mb_id'];
+
+// 프로젝트 목록 조회
+$sql = "SELECT * FROM g5_map_projects WHERE mb_id = '{$mb_id}' ORDER BY updated_at DESC";
+$result = sql_query($sql);
+
+$projects = [];
+while ($row = sql_fetch_array($result)) {
+    $projects[] = [
+        'id' => $row['project_id'],
+        'projectName' => $row['project_name'],
+        'mapType' => $row['map_type'],
+        'createdAt' => $row['created_at'],
+        'data' => json_decode($row['project_data'], true)
+    ];
+}
+
+echo json_encode([
+    'success' => true,
+    'projects' => $projects
+]);
+?>
